8a08f694f9bfde0b7f5a2f55bbdb7ee225d2e774
Renaldi <soeryadi.renaldi@gmail.com>
2022-06-14 18:26:59 +0700

    Tambah view + controller

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -13,14 +13,86 @@ class BarangController extends Controller
     }
 
     public function add() {
-        
-    }
+        $this->validate($request, [
+            'nama_barang' => 'required|string|max:155',
+            'stok' => 'required',
+            'status' => 'required',
+            'kode_kategori' => 'required'
+        ]);
+
+        $post = dataBarang::create([
+            'nama_barang' => $request->nama_barang,
+            'stok' => $request->stok,
+            'status' => $request->status,
+            'kode_kategori' => $request->kode_kategori
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'New post has been created successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again'
+                ]);
+             }
+        }
 
     public function edit(){
+        $this->validate($request, [
+            'nama_barang' => 'required|string|max:155',
+            'stok' => 'required',
+            'status' => 'required',
+            'kode_kategori' => 'required'
+        ]);
 
-    }
+        $post = dataBarang::FindOrFail($id);
+
+        $post = dataBarang::update([
+            'nama_barang' => $request->nama_barang,
+            'stok' => $request->stok,
+            'status' => $request->status,
+            'kode_kategori' => $request->kode_kategori
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'New post has been created successfully'
+                ]);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with([
+                    'error' => 'Some problem occurred, please try again'
+                ]);
+             }
+        }
 
     public function delete(){
+        $post = dataBarang::findOrFail($id);
+        $post->delete();
 
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'success' => 'Post has been deleted successfully'
+                ]);
+        } else {
+            return redirect()
+                ->route('post.index')
+                ->with([
+                    'error' => 'Some problem has occurred, please try again'
+                ]);
+             }
+        }
     }
-}
+
